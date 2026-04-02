0328dc2cd336e51fb4f0784da577379fec7da0d5
Kim Bogorad <kimbogorad@gmail.com>
2026-04-02 15:52:11 +0000

    Kim commit #5: Fixed example Echo server to work with STOMP to make the server connection testable

--- server/src/main/java/bgu/spl/net/impl/echo/EchoProtocol.java
+++ server/src/main/java/bgu/spl/net/impl/echo/EchoProtocol.java
@@ -1,22 +1,29 @@
 package bgu.spl.net.impl.echo;
 
-import bgu.spl.net.api.MessagingProtocol;
-import java.time.LocalDateTime;
-
-public class EchoProtocol implements MessagingProtocol<String> {
-
+import bgu.spl.net.api.StompMessagingProtocol; // Use the NEW interface
+import bgu.spl.net.srv.Connections;
+//שינינו את הדוגמה כדי שתתאים לפרוטוקול החדש
+public class EchoProtocol implements StompMessagingProtocol<String> {
+    //מוסיפים שדות שרלוונטיים לconnections
+    private int connectionId;
+    private Connections<String> connections;
     private boolean shouldTerminate = false;
 
+    //מוסיפים סטארט כי עד עכשיו לא היה צורך
     @Override
-    public String process(String msg) {
-        shouldTerminate = "bye".equals(msg);
-        System.out.println("[" + LocalDateTime.now() + "]: " + msg);
-        return createEcho(msg);
+    public void start(int connectionId, Connections<String> connections) {
+        this.connectionId = connectionId;
+        this.connections = connections;
     }
-
-    private String createEcho(String message) {
-        String echoPart = message.substring(Math.max(message.length() - 2, 0), message.length());
-        return message + " .. " + echoPart + " .. " + echoPart + " ..";
+    //process השתנה לvoid, כי שולחים הודעה ולא מחזירים הודעה.
+    @Override
+    public void process(String msg) {
+        if (msg.equals("..quit")) {
+            shouldTerminate = true;
+            return;
+        }
+        // Instead of 'return msg', we use the new way:
+        connections.send(connectionId, msg); 
     }
 
     @Override
